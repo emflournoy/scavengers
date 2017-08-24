d6787a9e84293c6d5d7d776d24db1ec27c449531
jmrigali <jmrigali@ymail.com>
2017-08-24 11:08:40 -0600

    changed login to portal added login for oauth

---
+++ src/components/portal/portal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Portal extends Component {
+  render() {
+    return (
+      <div className="Portal">
+
+      </div>
+    );
+  }
+}
+
+export default Portal;
