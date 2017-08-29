eb87e5d634b05c3ef1b6f0455b3f2a486eda5871
Elizabeth Flournoy <bflournoy@gmail.com>
2017-08-29 16:04:23 -0600

    spaced buttons

--- src/components/Hunt/huntpage.js
+++ src/components/Hunt/huntpage.js
@@ -34,9 +34,11 @@ class HuntPage extends Component{
         <div className='nav'>
         </div>
         <h3>these are all the clues.</h3>
-        {this.state.AllClues.map((ele, index)=>(
-          <AllClues data={ele} key={index} num={index+1}/>
-        ))}
+          <div className='allClueCards'>
+          {this.state.AllClues.map((ele, index)=>(
+            <AllClues data={ele} key={index} num={index+1}/>
+          ))}
+        </div>
       </div>
     )
   }
