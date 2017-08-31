70043d8f4abffb84c3b3c8e8d2fe34e218eb957a
jmrigali <jmrigali@ymail.com>
2017-08-31 14:27:32 -0600

    added react-router-do to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
